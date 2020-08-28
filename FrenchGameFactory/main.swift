eda6844e763d14c64a8a428f2e33bc5c52b3eb39
Pascal DIAMAND <pascal.diamand@gmail.com>
2020-08-28 15:33:12 +0200

    Initial Commit

---
+++ FrenchGameFactory/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FrenchGameFactory
+//
+//  Created by Pascal Diamand on 28/08/2020.
+//  Copyright © 2020 Pascal Diamand. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
